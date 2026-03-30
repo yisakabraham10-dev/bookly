2db5ab8b517876db39d669c9a8839c8e129a037d
Yisak Abraham <yisakabraham10@gmail.com>
2026-03-30 22:01:38 +0300

    transfered everything to linux safely.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from fastapi import FASTAPI
+from fastapi import FastAPI, status
+from fastapi.exceptions import HTTPException
+from typing import List
+from src.books.book_data import books
+from src.books.schemas import Book, UpdateBook
 
-app = FASTAPI()
-
-@app.get("/")
-async def read_root():
-    return{"message": "Hello world"}
-
-    
+app = FastAPI()
