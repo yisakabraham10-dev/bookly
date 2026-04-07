efb4ffa7e9da18aee3c5d51487c4fc2c6ad93e35
Yisak Abraham <yisakabraham10@gmail.com>
2026-04-07 20:09:36 +0300

    starting the project from the start because i am revising everything again.

--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI, status
-from fastapi.exceptions import HTTPException
-from typing import List
-from src.books.book_data import books
-from src.books.schemas import Book, UpdateBook
-
-app = FastAPI()
