d8b0e15c25b2f3dc62f42d7be860da604c310b40
Yisak Abraham <yisakabraham10@gmail.com>
2026-04-10 08:49:25 +0300

    added a  Books list for me to create api's for

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 from fastapi import FastAPI
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
 
 app = FastAPI()
 
-class Book(BaseModel):
-    title : str
-    author : str
-
-@app.get("/greet")
-async def return_hello( age: int = 0   , name: Optional[str] = "User") -> dict:
-    return{"message": f"Hello {name}.",  "age": age} 
-@app.post("/create_book")
-async def create_book(Book_data: Book) -> dict:
-    return {
-        "title": Book_data.title,
-        "author": Book_data.author
+Books = [
+    {
+        "book_id": 1,
+        "author": "George Orwell",
+        "title": "1984",
+        "publisher": "Secker & Warburg",
+        "published_date": "1949-06-08",
+        "page_count": 328,
+        "language": "English"
+    },
+    {
+        "book_id": 2,
+        "author": "Harper Lee",
+        "title": "To Kill a Mockingbird",
+        "publisher": "J.B. Lippincott & Co.",
+        "published_date": "1960-07-11",
+        "page_count": 281,
+        "language": "English"
+    },
+    {
+        "book_id": 3,
+        "author": "Paulo Coelho",
+        "title": "The Alchemist",
+        "publisher": "HarperTorch",
+        "published_date": "1988-01-01",
+        "page_count": 208,
+        "language": "Portuguese"
     }
+]
+
+class Book(BaseModel):
+    book_id: str
+    title: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+
+@app.get("/get_books")
+async def get_all_books()->dict :
+    return Books
