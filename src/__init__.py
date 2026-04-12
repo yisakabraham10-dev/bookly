79cfecec6bd6ab8d893a6d899fa60e2ce804a40b
Yisak Abraham <yisakabraham10@gmail.com>
2026-04-12 15:49:28 +0300

    was trying to fix why the fastapi app with the previous commit but i have found it now

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
 
-version = "version"
+version = "v1"
 app = FastAPI(version = version, 
               title = "bookly", 
               description= "A backend web service for books that is built through REST API's.")
 
-app.include_router(book_router, prefix= f"/api/{version}/book", tags= ["books"])
+app.include_router(book_router, prefix= f"/api/{version}/book", tags= ["books,"])
