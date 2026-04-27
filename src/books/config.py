5495e5a7c26c9d8cecd99ed44c39b1baa45dbadb
Yisak Abraham <yisakabraham10@gmail.com>
2026-04-27 05:15:31 +0300

    added an object for accessing env file

--- src/books/config.py
+++ src/books/config.py
@@ -2,3 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+
+    model_congig = SettingsConfigDict(
+        env_file= .env
+        extra= "ignore"
+    )
